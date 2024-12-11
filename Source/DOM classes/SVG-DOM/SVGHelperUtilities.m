2836ccc856582254aa6d4f4533ff3805dc7e5806
Oreliano <144818173+Oreliano@users.noreply.github.com>
2024-12-11 10:18:13 +0100

    Update SVGHelperUtilities.m

--- Source/DOM classes/SVG-DOM/SVGHelperUtilities.m
+++ Source/DOM classes/SVG-DOM/SVGHelperUtilities.m
@@ -289,6 +289,7 @@
 {
 	layer.name = nonStylableElement.identifier;
 	[layer setValue:nonStylableElement.identifier forKey:kSVGElementIdentifier];
+ 	[layer setValue:[nonStylableElement getAttribute:@"class"] forKey:kSVGElementClass];
 	
 #if FORCE_RASTERIZE_LAYERS
 	if ([layer respondsToSelector:@selector(setShouldRasterize:)]) {
